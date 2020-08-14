da847eb24f9d36d12762233b383f7a2f0c09e814
wattaihei <wattaihei.rapyuta@gmail.com>
2020-08-14 15:12:15 +0900

    new file

--- intern_hackathon/Screen/Favorite/FavoriteViewController.swift
+++ intern_hackathon/Screen/Favorite/FavoriteViewController.swift
@@ -1,9 +1,21 @@
 //
-//  FavoriteViewController.swift
+//  SearchResult.swift
 //  intern_hackathon
 //
-//  Created by 渡辺泰平 on 2020/08/14.
+//  Created by 渡辺泰平 on 2020/08/12.
 //  Copyright © 2020 caraquri. All rights reserved.
 //
 
-import Foundation
+import RealmSwift
+import SafariServices
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var events: [Event] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
